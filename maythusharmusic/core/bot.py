6f6030faef5a52eac9248af4c2b797ccb84fa70e
Wang Lin <maythusharmusic@gmail.com>
2025-11-25 07:57:17 +0630

    Update bot.py

--- maythusharmusic/core/bot.py
+++ maythusharmusic/core/bot.py
@@ -5,7 +5,7 @@ from pyrogram.enums import ChatMemberStatus, ParseMode
 import config
 from ..logging import LOGGER
 
-class Hotty(Client):
+class pisces(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
