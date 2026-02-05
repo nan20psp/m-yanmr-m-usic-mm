f26416930853b655f3163b3c31a885bd69cd679f
nan20psp <pyaesone10psp@gmail.com>
2026-02-05 08:31:01 +0630

    Update play.py

--- maythusharmusic/plugins/clone_plugins/play.py
+++ maythusharmusic/plugins/clone_plugins/play.py
@@ -24,7 +24,7 @@ from maythusharmusic.utils.inline import (
     track_markup,
 )
 from maythusharmusic.utils.logger import play_logs
-#from maythusharmusic.utils.activebotauto import ActiveBotAuto
+from maythusharmusic.utils.activebotauto import ActiveBotAuto
 from maythusharmusic.utils.stream.stream import stream
 from config import BANNED_USERS, lyrical
 
@@ -46,7 +46,7 @@ from config import BANNED_USERS, lyrical
     & ~BANNED_USERS
 )
 @PlayWrapper
-#@ActiveBotAuto
+@ActiveBotAuto
 async def play_commnd(
     client,
     message: Message,
