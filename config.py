878b256c9ecfa73aa33106b1b5532ffd087ade88
nan20psp <pyaesone10psp@gmail.com>
2026-02-05 11:06:03 +0630

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/nan20psp/m-yanmr-m-usic",
+    "https://github.com/nan20psp/m-yanmr-m-usic-mm",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
